e5c3bc24485512c51bf832083196c472271f6681
Juan Francisco Fonseca <52902579+FONSEX@users.noreply.github.com>
2021-07-07 08:42:40 -0400

    Add files via upload

---
+++ Data_Generated.sql
@@ -0,0 +1,88 @@
+
+
+
+
+INSERT INTO Animal
+VALUES('0001','Perro','Mamífero carnívoro de la familia de los cánidos, que constituye una subespecie del lobo.');
+
+INSERT INTO Animal
+VALUES ('0002','Gato','Mamífero carnívoro de la familia Felidae, es una subespecie domesticada por la convivencia con el ser humano.');
+
+INSERT INTO Animal
+VALUES ('0003','Hamster','Son una subfamilia de roedores, que se alimentan principalmente de semillas y granos.');
+
+
+INSERT INTO Raza
+VALUES ('1001','001','Golden Retriever','Es un perro dócil e inteligente, no presenta agresividad hacia las personas ni hacia otros perros, lo cual le convierte en un compañero perfecto para los más pequeños de la familia.','Inglaterra','Alto','S','27','67','37','G','Dorado Crema','01');
+
+INSERT INTO Raza
+VALUES ('1002','0001','Pastor Alemán','Excelente trabajador, guardián, afectuoso y paciente con los más pequeños de la familia.','Alemania','Alto','S','65','22','40','G','Negro con marcas claras','01');
+
+INSERT INTO Raza
+VALUES ('1003','0001','Beagle','Compacto y robusto, suelen ser alegres, equilibrados e inteligentes.','Inglaterra','Alto','N','33','44','10','P','Tricolor','01');
+
+INSERT INTO Raza
+VALUES ('1004','0001','Yorkshire Terrier','Muy vivaz y juguetón,de caracter equilibrado e inteligente está siempre alerta y es muy leal.','Inglaterra','Medio','N','40','30','3','P','Acero oscuro azulado','01');
+
+INSERT INTO Raza
+VALUES ('1005','0001','Boxer','Musculatura bien marcada, es un perro fuerte de estructura corta y compacta, es tranquilo y equilibrado.','Alemania','Medio','S','63','25','45','G','Leonado Atigrado','01');
+
+INSERT INTO Raza
+VALUES ('1006','0001','Poodle','Bien proporcionado y armónico, de apariencia inteligente y siempre alerta.', 'Francia','Medio','N','25','60','25','M','Negro, Blanco, Gris, Albaricoque o Marrón.','01');
+
+INSERT INTO Raza
+VALUES ('1007','0001','Bulldog','Destacan por su valentía y su habilidad como guardiantes, siempre alerta.','Inglaterra','Medio','S','20','40','23','M','Unicolor, Atigrado, Leonado','01');
+
+INSERT INTO Raza
+VALUES ('1008','0001','Pug Carlino','Son dóciles, confiados, amables y amistosos. Por otra parte, se trata de perros un poco testarudos.','China','Bajo','S','25','35','6','P',' Plata, Leonado, Albaricoque, Negro','01');
+
+INSERT INTO Raza
+VALUES ('1009','0001','Pitbull','Descendiente del Bull and Terrier, una mezcla entre el antiguo bulldog y el terrier.​ Su esperanza de vida es de 8 a 15 años.','Estados Unidos','Medio','S','40','50','14','G','Negro, Blanco, Pelaje atigrado, Cervato, Canela, Azul, Café, Gris, Rojo','01');
+
+INSERT INTO Raza
+VALUES ('1010','0001','Rottweiler','Son perros tranquilos, valientes, dóciles, amigables y obedientes. Su comportamiento con los niños es muy bueno y son excelentes guardianes, siempre atentos al entorno.','Alemania','Alto','S','56','63','40','G','Negros con marcas pardo-rojizas','01');
+
+INSERT INTO Raza
+VALUES ('1011','0002','Persa','Es una raza muy característica entre estos animales domésticos, teniendo unos rasgos identificativos que lo hacen distinguirlos con facilidad.','Persia','Medio','S','15','25','2','G','Blanco, negro, azul, rojo, crema, chinchilla, plata','02');
+
+INSERT INTO Raza
+VALUES ('1012','0002','Siamés','De tamaño mediano que tiene un cuerpo muy esbelto y una cola larga y estrecha hacia la punta.','Tailandia','Alto','N','15','25','2','M','Crema con rasgos negros','02');
+
+INSERT INTO Raza
+VALUES ('1013','0002','Ruso Azul','De carácter tranquilo y cariñoso. Aunque son algo tímidos con los desconocidos, con sus dueños son muy cercanos.','Rusia','Alto','N','20','25','3','M','Gris','02');
+
+INSERT INTO Raza
+VALUES ('1014','0002','Ragdoll','Cariñosísimos y dóciles, los Ragdoll son excelentes gatos domésticos ya que son muy familiares.','Estados Unidos','Bajo','N','20','30','6','G','Gris, Azul, Chocolate, o Lila, Rojo y Crema','02');
+
+INSERT INTO Raza
+VALUES ('1015','0002','Bobtail','Felino inteligente y amistoso se siente ofendido cuando no le prestan atención.','Japón','Alto','N','20','25','3','M','blanco, rojo, negro y carey','02');
+
+INSERT INTO Raza
+VALUES ('1016','0003','Ruso','Es una especie de roedor miomorfo de la familia Cricetidae, no se reconocen subespecies.','Rusia','Bajo','N','1','1','1','P','Gris, Perla, Zafiro','03');
+
+INSERT INTO Raza
+VALUES ('1017','0003','Sirio','En estado salvaje se considera en peligro de extinción, pero es popular como mascota y también se usa en la investigación científica','Siria','Bajo','N','1','1','1','M','Dorado con marcas blancas','03');
+
+INSERT INTO Raza
+VALUES ('1018','0003','Chino','Generalmente se utiliza como animal de compañía y para la experimentación en investigación biomédica.','China','Bajo','N','1','1','1','P','Gris,Blanco','03');
+
+INSERT INTO Raza
+VALUES ('1019','0003','Roborovski','Posee una cola corta y por la planta velluda de los miembros posteriores, es la especie de menor tamaño de su género','Mongolia','Bajo','N','1','1','1','P','Gris','03');
+
+INSERT INTO Raza
+VALUES ('1020','0003','Champbell','Uno de los mas populares y con más ejemplares en todo el mundo.','Rusia','Bajo','N','1','1','1','M','Dorado, Blanco','03');
+
+INSERT INTO Vacuna
+VALUES('91','45','Parvovirus','Trata el parvovirosis canina.');
+
+INSERT INTO Vacuna
+VALUES('76','150','Pentavalente','Inmuniza contra moquillo, adenovirus 1 y 2, parvovirus y parainfluenza (Canina).');
+
+INSERT INTO Vacuna
+VALUES('46','400','Antirrabica','Trata la rabia en todos los animales.');
+
+INSERT INTO Vacuna
+VALUES('30','350','Leucemia','Trata la leucemia en felinos.');
+
+INSERT INTO Vacuna
+VALUES('28','400','Trivalente','Protege contra Rinotraqueitis viral, Calicivirus y Panleucopenia (Felina).');
